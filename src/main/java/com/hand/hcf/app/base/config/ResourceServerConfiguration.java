16556451b07fcc639a656d90b3c3e86a77b6ee4e
huweishan <weishan.hu@hand-china.com>
2019-03-12 21:36:17 +0800

    统一oauth异常消息格式

--- src/main/java/com/hand/hcf/app/base/config/ResourceServerConfiguration.java
+++ src/main/java/com/hand/hcf/app/base/config/ResourceServerConfiguration.java
@@ -2,13 +2,12 @@
 
 package com.hand.hcf.app.base.config;
 
+import com.hand.hcf.core.config.AuthExceptionEntryPoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by markfredchen on 23/06/2017.
@@ -17,12 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         //@formatter:off
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthExceptionEntryPoint())
                 .and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
---
+++ src/main/java/com/hand/hcf/app/base/exception/ExtOAuthException.java
@@ -0,0 +1,12 @@
+package com.hand.hcf.app.base.exception;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
+
+@JsonSerialize(using = OAuthExceptionSerializer.class)
+public class ExtOAuthException extends OAuth2Exception {
+
+    public ExtOAuthException(String msg) {
+        super(msg);
+    }
+}
